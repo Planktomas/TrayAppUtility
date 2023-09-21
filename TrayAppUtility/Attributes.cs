36d8f6154fc1570e352a6a49896bfa4e9dc8cca5
Tomas Kucinskas <tomaskucinskas202@gmail.com>
2023-09-21 22:27:22 +0300

    Add Autorun attribute

--- TrayAppUtility/Attributes.cs
+++ TrayAppUtility/Attributes.cs
@@ -10,4 +10,15 @@ namespace TrayAppUtility
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class NoLogAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class AutorunAttribute : Attribute 
+    {
+        public string TimeSpanString { get; }
+
+        public AutorunAttribute(string timeSpanString)
+        {
+            TimeSpanString = timeSpanString;
+        }
+    }
 }
